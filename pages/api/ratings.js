e6c0e3f0e7fad2508f49c9695fb527013b31ce6f
LaggySon <56988314+LaggySon@users.noreply.github.com>
2022-07-20 16:17:38 -0400

    api auth fix 4

--- pages/api/ratings.js
+++ pages/api/ratings.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import prisma from "../../lib/prisma.js";
 import { builtinModules } from "module";
-import { getServerSession } from "next-auth";
+import { getSession } from "next-auth/client";
 // import { authOptions } from "./api/auth/[...nextauth]";
 
 export default async function handler(req, res) {
